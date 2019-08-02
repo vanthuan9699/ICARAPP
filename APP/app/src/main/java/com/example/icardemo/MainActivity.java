0ab516f9fd8e977fcc401a8eaeefbfe40615a365
thuanclub <vanthuan080896@gmail.com>
2019-08-02 16:19:23 +0700

    commit

---
+++ APP/app/src/main/java/com/example/icardemo/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.icardemo;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtUser, edtPass;
+    Button btnDangNhap, btnExit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        ControlButton();
+    }
+
+    private void ControlButton() {
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog);
+                builder.setTitle("Bạn có muốn thoát");
+                builder.setMessage("Hãy lựa chọn bên dưới để xác nhận");
+                builder.setIcon(android.R.drawable.ic_dialog_alert);
+                builder.setPositiveButton("có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        onBackPressed();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+    private void init() {
+        edtUser = (EditText)findViewById(R.id.edt_user);
+        edtPass = (EditText)findViewById(R.id.edt_pass);
+        btnDangNhap = (Button)findViewById(R.id.btn_dangnhap);
+        btnExit = (Button)findViewById(R.id.btn_exit);
+    }
+}
